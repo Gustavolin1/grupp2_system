afa2390f7f0f7554e79a3d795eb885b9bcc3320c
Gustav.Olin <olin.gustav03@gmail.com>
2026-02-25 10:59:14 +0100

    Skapa vy och controller class för hytt

--- src/main/java/Hytt/HyttalternativController.java
+++
@@ -1,4 +0,0 @@
-package Hytt;
-
-public class HyttalternativController {
-}
---
+++ src/main/java/com/example/grupp2_system/HyttalternativController.java
@@ -0,0 +1,22 @@
+package com.example.grupp2_system;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+
+public class HyttalternativController {
+
+    @FXML
+    private Button btnBack;
+
+    @FXML
+    private Button btnSave;
+
+    @FXML
+    private ChoiceBox<?> chHytt;
+
+    @FXML
+    private Label lblHytt;
+
+}
