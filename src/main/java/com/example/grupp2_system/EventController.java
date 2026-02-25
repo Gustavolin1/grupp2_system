f549526b4d55d0415013a6b05a196713a743c3d2
John <john.oberg.pf@gmail.com>
2026-02-25 11:54:50 +0100

    Implemented Event Information view with controllers that leads nowhere.

---
+++ src/main/java/com/example/grupp2_system/EventController.java
@@ -0,0 +1,25 @@
+package com.example.grupp2_system;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.text.Text;
+
+public class EventController {
+
+    @FXML
+    private Menu mnbarEvenmang;
+
+    @FXML
+    private MenuItem mnbuttonBokningsbara;
+
+    @FXML
+    private MenuItem mnbuttonEvent;
+
+    @FXML
+    private MenuItem mnbuttonTillgängliga;
+
+    @FXML
+    private Text txtEvenemangInfo;
+
+}
