95769e916f7f6af107f8e908599a7b4574c6c4ea
Ida <ida.olsson96@hotmail.com>
2026-02-25 11:35:40 +0100

    Foodinto view och controller skapat. Med funktionalitet fram och tillbaka till startsidan

--- src/main/java/com/example/grupp2_system/InfoFoodController.java
+++ src/main/java/com/example/grupp2_system/InfoFoodController.java
@@ -7,31 +7,38 @@ import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloController {
+public class InfoFoodController {
 
     @FXML
-    private Button btnmatinfo;
+    private Button btntillbaka;
 
     @FXML
-    private Label welcomeText;
+    private Label lblMatpaket;
 
     @FXML
-    void onHelloButtonClick(ActionEvent event) {
-
-    }
+    private Text txtbudgetmat;
 
     @FXML
-    public void openFoodinfoScene(ActionEvent event) throws IOException{
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("food-info-view.fxml"));
+    private Text txtlyxmat;
+
+    @FXML
+    private Text txtmellanmat;
+
+    @FXML
+    private Text txtprislistamat;
+
+    @FXML
+    public void openHomeScene(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
         Scene chosenScene = new Scene(loader.load(),800, 600);
 
         Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
         window.setScene(chosenScene);
         window.show();
     }
-
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.example.grupp2_system {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.base;
+    requires javafx.graphics;
 
 
     opens com.example.grupp2_system to javafx.fxml;
