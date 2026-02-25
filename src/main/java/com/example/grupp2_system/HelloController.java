c80102745a6390aef012e387b666d6eb46fc46e6
Gustav.Olin <olin.gustav03@gmail.com>
2026-02-25 11:26:20 +0100

    Fixade byte av Scene

--- src/main/java/com/example/grupp2_system/HelloController.java
+++ src/main/java/com/example/grupp2_system/HelloController.java
@@ -1,7 +1,15 @@
 package com.example.grupp2_system;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Stack;
 
 public class HelloController {
     @FXML
@@ -10,5 +18,16 @@ public class HelloController {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!!");
+
+
+    }
+    public void OpenSecondScene(ActionEvent event) throws IOException{
+
+        FXMLLoader loader = new FXMLLoader((getClass().getResource("Hyttalternativ.fxml")));
+        Scene chosenScene = new Scene(loader.load(), 600, 400);
+
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(chosenScene);
+        window.show();
     }
 }
