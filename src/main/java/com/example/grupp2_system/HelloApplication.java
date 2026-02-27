d187722e0a355092863009941ba9b6ef8987e529
Gustav.Olin <olin.gustav03@gmail.com>
2026-02-27 11:09:47 +0100

    La till informationscen och controller, även en SceneManager som hanterar scener, byte och gå tillbaka. Lagras på en stack.

--- src/main/java/com/example/grupp2_system/HelloApplication.java
+++ src/main/java/com/example/grupp2_system/HelloApplication.java
@@ -9,15 +9,20 @@ import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.    class.getResource("Startpage.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage stage) throws Exception {
 
+        SceneManager.setStage(stage);
+
+        FXMLLoader loader = new FXMLLoader(
+                getClass().getResource(
+                        "/com/example/grupp2_system/Startpage.fxml"));
+
+        stage.setScene(new Scene(loader.load()));
+        stage.setTitle("Bokningssystem");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
-
-
-//hello
---
+++ src/main/java/com/example/grupp2_system/InformationPageController.java
@@ -0,0 +1,48 @@
+package com.example.grupp2_system;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class InformationPageController {
+
+    @FXML
+    private Button BtnBack;
+
+    @FXML
+    private Button BtnEvent;
+
+    @FXML
+    private Button btnHytt;
+
+    @FXML
+    private Button btnMat;
+
+    public void BtnBack(ActionEvent actionEvent) {
+    }
+
+    public void BtnEvent(ActionEvent actionEvent) {
+
+    }
+
+    public void openEvent(ActionEvent event) throws IOException
+    {
+    SceneManager.switchScene("EventInformation.fxml");
+    }
+    public void openFoodInfo(ActionEvent event) throws IOException
+    {
+    SceneManager.switchScene("food-info-view.fxml");
+
+    }
+    public void backPage(ActionEvent event) throws IOException
+    {
+        SceneManager.goBack();
+    }
+}
+
