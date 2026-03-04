a82c9c0831ede0b8a64e0b2a747572b5845a64cf
Ida <ida.olsson96@hotmail.com>
2026-03-04 18:58:33 +0100

    Fixade Checkdata utseende, ändra till svenska, ändrade i controllern så att ett bekräftelse meddelande kommer upp

--- src/main/java/com/example/grupp2_system/CheckDataController.java
+++ src/main/java/com/example/grupp2_system/CheckDataController.java
@@ -5,6 +5,7 @@ import com.example.grupp2_system.Booking.BookingManager;
 import com.example.grupp2_system.SceneManager.SceneManager;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
@@ -37,32 +38,48 @@ public class CheckDataController {
     private Label lblfoodoption;
 
     @FXML
-    public void initialize()
-    {
+    public void initialize() {
         Booking booking = SceneManager.getCurrentBooking();
-        lblID.setText(booking.getBookingId());
-        lbldateto.setText(booking.getDateThere().toString());
-        lblDatefrom.setText(booking.getDateHome().toString());
-        lblcabin.setText(booking.getCabinThere());
-        lblfoodoption.setText(booking.getFoodThere());
-        lblEvents.setText(String.join(", ", booking.getEvents()));
 
-
-
+        if (booking != null) {
+            lblID.setText("Bokningsnummer: " + booking.getBookingId());
+            lbldateto.setText("Avresa: " + booking.getDateThere().toString());
+            lblDatefrom.setText("Hemresa: " + booking.getDateHome().toString());
+            lblcabin.setText("Kabin: " + booking.getCabinThere());
+            lblfoodoption.setText("Matval: " + booking.getFoodThere());
+            lblEvents.setText("Bokade evenemang: " + String.join(", ", booking.getEvents()));
+        }
     }
+
     @FXML
-    public void setBtnBack(ActionEvent event)throws IOException
-    {
+    public void setBtnBack(ActionEvent event) throws IOException {
         SceneManager.goBack();
     }
-    @FXML
-    public void setBtnSave(ActionEvent event)throws IOException
-    {
-        Booking booking = SceneManager.getCurrentBooking();
-        BookingManager bookingManager = new BookingManager();
 
+    @FXML
+    public void setBtnSave(ActionEvent event) throws IOException {
+        Booking booking = SceneManager.getCurrentBooking();
+
+        if (booking == null) {
+            Alert errorAlert = new Alert(Alert.AlertType.ERROR);
+            errorAlert.setTitle("Fel");
+            errorAlert.setHeaderText(null);
+            errorAlert.setContentText("Ingen bokning att spara.");
+            errorAlert.showAndWait();
+            return;
+        }
+
+        // Spara bokningen
         BookingManager.saveBooking(booking);
+
+        // Visa bekräftelse
+        Alert infoAlert = new Alert(Alert.AlertType.INFORMATION);
+        infoAlert.setTitle("Bokning sparad");
+        infoAlert.setHeaderText(null);
+        infoAlert.setContentText("Din bokning har sparats!");
+        infoAlert.showAndWait();
+
+        // Gå tillbaka till start sidan
         SceneManager.switchScene("Startpage.fxml");
     }
-
 }
