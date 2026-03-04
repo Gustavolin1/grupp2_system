4ca0175979fae93fbef40b0794651896af4c7ad6
Gustav.Olin <olin.gustav03@gmail.com>
2026-03-04 10:59:46 +0100

    Avbryt bokning i Hyttalternativ.fxml

--- src/main/java/com/example/grupp2_system/HyttalternativController.java
+++ src/main/java/com/example/grupp2_system/HyttalternativController.java
@@ -9,6 +9,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public class HyttalternativController {
 
@@ -28,12 +29,11 @@ public class HyttalternativController {
     private Button btninfo;
 
     @FXML
-    private ToggleGroup grpOption;
+    private Button btnCancel;
 
     @FXML
-    private void Initialize()
-    {
-    }
+    private ToggleGroup grpOption;
+
     @FXML
     public void next(ActionEvent event) throws IOException
     {
@@ -58,6 +58,21 @@ public class HyttalternativController {
         SceneManager.switchScene("Hyttinformation.fxml");
     }
 
+    @FXML
+    private void cancelBooking() throws IOException {
 
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Avbryt bokning");
+        alert.setHeaderText("Är du säker?");
+        alert.setContentText("All information du fyllt i kommer att raderas.");
 
+        Optional<ButtonType> result = alert.showAndWait();
+
+        if (result.isPresent() && result.get() == ButtonType.OK) {
+
+            SceneManager.setCurrentBooking(null);
+
+            SceneManager.switchScene("StartPage.fxml");
+        }
+    }
 }
