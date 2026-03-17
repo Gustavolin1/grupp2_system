dac461240044df17f59e276a85514f63cd8b2de1
Gustav.Olin <olin.gustav03@gmail.com>
2026-03-17 15:48:46 +0100

    fix jar name

--- Scripts/build-windows.sh
+++ Scripts/build-windows.sh
@@ -1,24 +1,34 @@
 @echo off
 
 set APP_NAME=MarsTravelsBooking
-set ICON=..\icons\icon.ico
+set VERSION=0.1.0
+set FINAL_NAME=%APP_NAME%-%VERSION%
 
-set INPUT=..\out\artifacts\grupp2_system_jar
+set ICON=icons\icon.ico
+
+set INPUT=out\artifacts\grupp2_system_jar
 set JAR=grupp2_system.jar
 set MAIN_CLASS=com.example.grupp2_system.App.Launcher
 
+set DEST=builds
+
 echo Rensar gamla builds...
-if exist ..\%APP_NAME%.exe rmdir /s /q ..\%APP_NAME%.exe
+if exist %DEST%\%FINAL_NAME%.exe rmdir /s /q %DEST%\%FINAL_NAME%.exe
+
+echo Skapar builds-mapp...
+if not exist %DEST% mkdir %DEST%
 
 echo Bygger EXE...
 jpackage ^
 --input %INPUT% ^
---name %APP_NAME% ^
+--dest %DEST% ^
+--name %FINAL_NAME% ^
+--app-version %VERSION% ^
 --main-jar %JAR% ^
 --main-class %MAIN_CLASS% ^
 --icon %ICON% ^
 --type exe
 
 echo.
-echo KLART! EXE skapad
+echo KLART! (Windows build i /builds)
 pause
